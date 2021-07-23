dd3ef0ef55ca18336a1b5f5ab9a3d44582de492f
SHAINA <SHAINA@LAPTOP-HHC7S5DA>
2021-07-23 14:56:01 +0530

    1St commit

---
+++ src/casetoolsexam/Fruit_103.java
@@ -0,0 +1,15 @@
+package casetoolsexam;
+
+public class Fruit_103 {
+	String color;
+	Fruit_103(String color)
+	{
+	this.color=color;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
